50329bc5e5816a45e4452896e4c3946c53324d35
Fuskia <ing.silviadinardo@gmail.com>
2013-05-11 18:52:30 +0200

    es3

--- 2013-05-10/python/exercise3.py
+++ 2013-05-10/python/exercise3.py
@@ -1,3 +1,48 @@
+from pyplasm import *
+import scipy
+from scipy import *
+
+#---------------------------------------------------------
+def VERTEXTRUDE((V,coords)):
+    return CAT(AA(COMP([AA(AR),DISTR]))(DISTL([V,coords])))
+
+def cumsum(iterable):
+    # cumulative addition: list(cumsum(range(4))) => [0, 1, 3, 6]
+    iterable = iter(iterable)
+    s = iterable.next()
+    yield s
+    for c in iterable:
+        s = s + c
+        yield s
+
+def larExtrude(model,pattern):
+    V,FV = model
+    d = len(FV[0])
+    offset = len(V)
+    m = len(pattern)
+    outcells = []
+    for cell in FV:
+        # create the indices of vertices in the cell "tube"
+        tube = [v + k*offset for k in range(m+1) for v in cell]
+        # take groups of d+1 elements, via shifting by one
+        rangelimit = len(tube)-d
+        cellTube = [tube[k:k+d+1] for k in range(rangelimit)]
+        outcells += [scipy.reshape(cellTube,newshape=(m,d,d+1)).tolist()]
+    outcells = AA(CAT)(TRANS(outcells))
+    outcells = [group for k,group in enumerate(outcells) if pattern[k]>0 ]
+    coords = list(cumsum([0]+(AA(ABS)(pattern))))
+    outVerts = VERTEXTRUDE((V,coords))
+    newModel = outVerts, CAT(outcells)
+    return newModel
+
+def GRID(args):
+    model = ([[]],[[0]])
+    for k,steps in enumerate(args):
+        model = larExtrude(model,steps*[1])
+    V,cells = model
+    verts = AA(list)(scipy.array(V) / AA(float)(args))
+    return MKPOL([verts, AA(AA(lambda h:h+1))(cells), None])
+
 
 dom1D = INTERVALS(1)(32)
 
